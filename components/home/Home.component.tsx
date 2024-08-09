7b2db4ae800156dd6672d4d0d165551ebd11f606
JQeybulla <geybulla.aliverdiyev.std@bhos.edu.az>
2024-08-09 16:37:38 +0400

    added pagination to course card

--- components/home/Home.component.tsx
+++ components/home/Home.component.tsx
@@ -6,9 +6,9 @@ import courseImage3 from './../../public/images/course3.jpg';
 import courseImage4 from './../../public/images/course4.jpg';
 import courseImage5 from './../../public/images/course5.jpg';
 import courseImage6 from './../../public/images/course6.jpg';
-import {Rate} from "antd";
+import {Pagination, Rate} from "antd";
 import FiltersComponent from "@/components/filters/Filters.component";
-import {FilterFilled} from "@ant-design/icons";
+import PaginationComponent from "@/components/pagination/Pagination.component";
 
 const courses = [
   {id: 1, name: 'Frontend təlimi', image: courseImage},
@@ -26,7 +26,7 @@ export default function HomeComponent() {
     <div className={`${styles.homeContainer} px-20 py-10`}>
       <h1 className={styles.header}>Populyar kurslar</h1>
       <FiltersComponent />
-      <div className={`${styles.home} grid grid-cols-4 gap-8 py-4`}>
+      <div className={`${styles.home} grid sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-12 2xl:grid-cols-5 py-4`}>
         {courses.map((item, index) => (
           <div key={item.id} className={`${styles.course}`}>
             <Image src={item.image} alt={'image'} />
@@ -45,6 +45,7 @@ export default function HomeComponent() {
           </div>
         ))}
       </div>
+      <PaginationComponent />
     </div>
   )
 }
---
+++ components/pagination/Pagination.component.tsx
@@ -0,0 +1,13 @@
+import styles from './Pagination.module.scss';
+
+export default function PaginationComponent() {
+  return (
+    <div className={`${styles.pagination}`}>
+      <div className={`${styles.item} ${styles.active}`}>1</div>
+      <div className={styles.item}>2</div>
+      <div className={styles.item}>3</div>
+      <div className={styles.item}>4</div>
+      <div className={styles.item}>5</div>
+    </div>
+  )
+}
